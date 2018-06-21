b809420e5227864a5c9f8abdd710bce886861800
Ben Lalanes <chtka.ow@gmail.com>
2018-06-21 10:10:03 -0700

    Added 'number_of_sites' field

--- searchers/clinical_trials_searcher.py
+++ searchers/clinical_trials_searcher.py
@@ -46,19 +46,36 @@ class ClinicalTrialsSearcher(Searcher):
             root = ET.fromstring(file_as_string)
             
             # get all desired data
+            
+            # ct_id
             ct_id = root.find('id_info').find('nct_id').text
+
+            # title
             title = root.find('official_title').text if root.find('official_title') is not None else root.find('brief_title').text if root.find('brief_title') is not None else None
+            
+            # status
             status = root.find('overall_status').text if root.find('overall_status') is not None else None
+            
+            # principal_investigator
             overall_official = root.find('overall_official')
             if overall_official:
                 principal_investigator = overall_official.find('last_name').text
+            
+            # number_of_sites
+            number_of_sites = len(root.findall('location'))
+
+            # lead_sponsor
             lead_sponsor = root.find('sponsors').find('lead_sponsor').find('agency').text
+            
+            # collaborators
             collaborators = [collaborator.find('agency').text for collaborator in root.find('sponsors').findall('collaborator')]
+            
+            # estimated_completion_date
             estimated_completion_date = root.find('primary_completion_date').text if root.find('primary_completion_date') is not None else None
 
             # create row for insertion
             row = pd.Series({"ct_id": ct_id, "title": title, "status": status, "principal_investigator": principal_investigator,
-                         "lead_sponsor": lead_sponsor, "collaborators": collaborators,
+                         "number_of_sites": number_of_sites, "lead_sponsor": lead_sponsor, "collaborators": collaborators,
                          "estimated_completion_date": estimated_completion_date})
 
             # insert row into DataFrame
