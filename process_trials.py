a07ed2885c2a48cca97f2d3ace9a924a97654d67
Benjamin Llanes <Benjamin.Llanes@resmed.com>
2018-06-26 15:53:21 -0700

    Added processing data and uploading to S3 bucket for processed data.

--- process_trials.py
+++ process_trials.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 from processors.isrctn_processor import ISRCTNProcessor
@@ -7,19 +7,41 @@ from processors.clinical_trials_processor import ClinicalTrialsProcessor
 
 from search_trials import CLINICAL_TRIALS_DATA_BUCKET_NAME
 
-processor = ANZCTRProcessor()
+processor = ClinicalTrialsProcessor()
 
 import boto3
 import io
 
-buffer = io.BytesIO()
+# buffer = io.BytesIO()
+
+# s3 = boto3.resource('s3')
+
+# obj = s3.Object(CLINICAL_TRIALS_DATA_BUCKET_NAME, 'ANZCTR/resmed/2018/06/26/anzctr_results_resmed.zip')
+# obj.download_fileobj(buffer)
+
+# df = processor.process_and_load_df(buffer)
+
+# print(df.head())
 
 s3 = boto3.resource('s3')
+bucket = s3.Bucket(CLINICAL_TRIALS_DATA_BUCKET_NAME)
 
-obj = s3.Object(CLINICAL_TRIALS_DATA_BUCKET_NAME, 'ANZCTR/resmed/2018/06/26/anzctr_results_resmed.zip')
-obj.download_fileobj(buffer)
+for obj_summary in bucket.objects.all():
+    buffer = io.BytesIO()
+    obj = obj_summary.Object()
 
-df = processor.process_and_load_df(buffer)
+    print('Processing %s' % obj.key)
 
-print(df.head())
+    obj.download_fileobj(buffer)
+
+    df = processor.process_and_load_df(buffer)
+
+    csv_buffer = io.StringIO()
+
+    df.to_csv(csv_buffer)
+
+    s3.Object('clinical-trials-analysis-data-postprocessing', os.path.splitext(obj_summary.key)[0] + '.csv').put(Body=csv_buffer.getvalue())
+
+
+
 
