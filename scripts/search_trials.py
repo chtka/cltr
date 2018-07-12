20ca737d0a48aa1937191e0362c399d79fe3bdeb
Benjamin Llanes <Benjamin.Llanes@resmed.com>
2018-07-12 14:06:50 -0700

    Refactored search_trials.py to make it suitable for testing; added test case for search_trials.py.

--- scripts/search_trials.py
+++ scripts/search_trials.py
@@ -18,43 +18,7 @@ from searchers.isrctn_searcher import ISRCTNSearcher
 
 RAW_DATA_FORMAT_STRING = "%d/%02d/%02d/%s/%s/%s"
 
-def search_trials(terms, site_name, Searcher, logger):
-    """
-    Uses the specified Searcher to query the given site using the list of
-    query terms. Downloads the raw data and uploads it to S3.
-
-    Args:
-        terms: The list of terms to search for.
-        site_name: The website/database being searched.
-        Searcher: The type of searcher to use for searching.
-
-    Returns:
-        None.
-    """
-    with Searcher() as searcher:
-        for term in terms:
-
-            # download the raw data
-            filepath = searcher.search_and_download_raw(term)
-            if filepath:
-                
-                print(hostname, "[%s]" % datetime.datetime.utcnow(), "DOWNLOAD", filepath)
-                logger.info("%s [%s] DOWNLOAD %s" % (hostname, datetime.datetime.utcnow(), filepath))
-                
-                # upload file and remove local copy
-                filepath_s3 = RAW_DATA_FORMAT_STRING % (d.year, d.month, d.day, site_name, term, filepath)
-                bucket.upload_file(os.path.join(os.getcwd(), filepath), filepath_s3)
-                os.remove(os.path.join(os.getcwd(), filepath))
-                
-                print(hostname, "[%s]" % datetime.datetime.utcnow(), "UPLOAD", filepath_s3)
-                logger.info("%s [%s] UPLOAD %s\n" % (hostname, datetime.datetime.utcnow(), filepath_s3))
-            
-            else:
-
-                print(hostname, "[%s]" % datetime.datetime.utcnow(),  "FAILED_SEARCH", site_name, "+", term)
-                logger.warning("%s [%s] FAILED_SEARCH: %s + %s" % (hostname, datetime.datetime.utcnow(), site_name, term))
-
-if __name__ == "__main__":
+def run_search_trials(config_path, no_upload=False):
 
     hostname = gethostname()
 
@@ -71,7 +35,7 @@ if __name__ == "__main__":
     searchers = dict()
 
     # config
-    with open('config/config.json') as config_data_file:
+    with open(config_path) as config_data_file:
         data = json.load(config_data_file)
 
         search_terms_file_path = data['search_terms_file_path']
@@ -101,12 +65,43 @@ if __name__ == "__main__":
             terms.append(term.rstrip())
 
     for searcher_name in searchers:
-        search_trials(terms, searcher_name, searchers[searcher_name], logger)
 
-    
+        with searchers[searcher_name]() as searcher:
+            
+            for term in terms:
+
+                filepath = searcher.search_and_download_raw(term)
+                if filepath:
+                    print(hostname, "[%s]" % datetime.datetime.utcnow(), "DOWNLOAD", filepath)
+                    logger.info("%s [%s] DOWNLOAD %s" % (hostname, datetime.datetime.utcnow(), filepath))
+
+
+                    if not no_upload:
+                        
+                        filepath_s3 = RAW_DATA_FORMAT_STRING % (d.year, d.month, d.day, searcher_name, term, filepath)
+
+                        bucket.upload_file(os.path.join(os.getcwd(), filepath), filepath_s3)
+
+                        print(hostname, "[%s]" % datetime.datetime.utcnow(), "UPLOAD", filepath_s3)
+                        logger.info("%s [%s] UPLOAD %s\n" % (hostname, datetime.datetime.utcnow(), filepath_s3))
+
+                    os.remove(os.path.join(os.getcwd(), filepath))
+
+                
+                else:
+
+                    print(hostname, "[%s]" % datetime.datetime.utcnow(),  "FAILED_SEARCH", searcher_name, "+", term)
+                    logger.warning("%s [%s] FAILED_SEARCH: %s + %s" % (hostname, datetime.datetime.utcnow(), searcher_name, term))
+
     print(hostname, "[%s]" % datetime.datetime.utcnow(), "STOP", __file__)
     logger.info("%s [%s] STOP %s" % (hostname, datetime.datetime.utcnow(), __file__))
 
-    log_file_path = "%d/%02d/%02d/search_trials_%s.log" % (d.year, d.month, d.day, gethostname())
+    if not no_upload:
 
-    s3.Object(data['bucket_names']['raw_data_bucket_name'], log_file_path).put(Body=log_buffer.getvalue())
+        log_file_path = "%d/%02d/%02d/search_trials_%s.log" % (d.year, d.month, d.day, gethostname())
+
+        s3.Object(data['bucket_names']['raw_data_bucket_name'], log_file_path).put(Body=log_buffer.getvalue())
+
+
+if __name__ == "__main__":
+    run_search_trials('config/config.json')
--- tests/test_searchers.py
+++ tests/test_searchers.py
@@ -4,12 +4,32 @@ from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import unittest
+import json
 
 from searchers.anzctr_searcher import ANZCTRSearcher
 from searchers.clinical_trials_searcher import ClinicalTrialsSearcher
 from searchers.isrctn_searcher import ISRCTNSearcher
 
+from scripts.search_trials import run_search_trials
+
 class TestSearchers(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        fake_config = {
+            "bucket_names": {
+                "raw_data_bucket_name": "fake_raw_data_bucket",
+                "processed_data_bucket_name": "fake_processed_data_bucket"
+            },
+            "search_terms_file_path": path.join(path.dirname(path.abspath(__file__)), 'test_data/search_terms_short.txt'),
+            "searchers": ["clinical-trials-gov"]
+        }
+
+        cls.fake_config_filepath = path.join(path.dirname(path.abspath(__file__)), 'test_data/config.json')
+
+        with open(cls.fake_config_filepath, 'w') as fake_config_file:
+            json.dump(fake_config, fake_config_file)
+            
 
     def test_clinical_trials_searcher(self):
         QUERY = "The Sleep, Liver Evaluation and Effective Pressure Study"
@@ -38,6 +58,15 @@ class TestSearchers(unittest.TestCase):
         finally:
             if path.exists(DOWNLOAD_FILE_FORMAT % QUERY):
                 os.remove(DOWNLOAD_FILE_FORMAT % QUERY)
+
+    def test_search_trials_script(self):
+
+        run_search_trials(self.fake_config_filepath, no_upload=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.fake_config_filepath)
+
 
 
 
