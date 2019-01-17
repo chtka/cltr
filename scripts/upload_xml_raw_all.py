87a8bfd8b36c18f75eff59627e12a1f2e2231baa
benlalanes <bllanes@ucsd.edu>
2019-01-17 15:44:42 -0800

    Added regex extraction of xml file name.

--- scripts/upload_xml_raw_all.py
+++ scripts/upload_xml_raw_all.py
@@ -1,8 +1,9 @@
 import boto3
 import os
+import re
 import zipfile
 
-from io import BytesIO, StringIO
+from io import BytesIO
 from socket import gethostname
 from urllib.request import urlopen
 
@@ -15,13 +16,18 @@ ALL_CT_GOV_TRIALS_URL = 'https://clinicaltrials.gov/AllPublicXML.zip'
 
 # Initialize S3 resources.
 s3 = boto3.client('s3', region_name=AWS_REGION)
+
+pattern = re.compile(r'.+\/(.+\.xml)')
+
 print("Downloading zip archive...")
 with urlopen(ALL_CT_GOV_TRIALS_URL) as resp:
   archive = zipfile.ZipFile(BytesIO(resp.read()))
 
   for xml_file_name in [name for name in archive.namelist() if name.endswith('.xml')]:
+
+    shortFileName = pattern.match(xml_file_name)
     
     with archive.open(xml_file_name, 'r') as xml_file:
       print("Uploading %s.xml" % xml_file_name)
-      s3.put_object(Body=xml_file.read(), Bucket=RAW_XML_S3_BUCKET, Key=xml_file_name)
+      s3.put_object(Body=xml_file.read(), Bucket=RAW_XML_S3_BUCKET, Key=shortFileName)
 
